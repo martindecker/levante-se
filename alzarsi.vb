8b50756e1b8b162109befced5f5dec5a8b3fc1c6
martindecker <c-m-2005@onlinehome.de>
2018-08-24 19:44:19 +0200

    Update alzarsi.vb

--- alzarsi.vb
+++ alzarsi.vb
@@ -1,13 +1,27 @@
 Imports System
 
+
 Module Program
+    Function janein1(text As String)
+        Dim line As String
+        Do
+            Console.Write("{0} ? (j/n) ", text)
+            line = Console.ReadLine()
+        Loop Until "jnJN".Contains(line)
+        Return line.ToUpper() = "J"
+    End Function
+
+    Public todo3 = VbLf
+
     Sub Main(args As String())
-    For i As Integer = 0 To 4
-                        Console.WriteLine(i)
-                    Next
-    For i As Integer = 1 to 9 step 2
-                        Console.WriteLine(i)
-                    Next
+        Dim stoerungen1 = janein1("Lärm hörbar")
+        Dim stoerungen2 = If(stoerungen1 = False, False, janein1("Straße arg befahren"))
+        If stoerungen2 AndAlso DateTime.Today.DayOfWeek = DayOfWeek.Saturday Then
+            todo3 = todo3 & "Ursache fuer stark befahrene Straße herausfinden"
+        Else
+            Console.WriteLine("nix")
+        End If
+        Console.WriteLine(todo3)
         Console.WriteLine("I began the project on August 22, 2018. Please enter sth to end the program.")
         Console.ReadKey()
     End Sub
