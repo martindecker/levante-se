e4f2123377bb4ecd533dbbee6756353af22c4b55
martindecker <c-m-2005@onlinehome.de>
2019-12-16 23:54:20 +0100

    Use dotnet-core 3.1

--- build-wind.bat
+++ build-wind.bat
@@ -1,5 +1,5 @@
 rem Build on Windows
 cd dotnet
 dotnet build
-certutil -hashfile bin\Debug\netcoreapp3.0\levante-se.exe >bin\Debug\netcoreapp3.0\lev_hash.txt
+certutil -hashfile bin\Debug\netcoreapp3.1\levante-se.exe >bin\Debug\netcoreapp3.1\lev_hash.txt
 pause
